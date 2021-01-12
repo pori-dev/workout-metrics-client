2e2dad684df0a9de66e48536786179be8cceee31
pooriya <pooriya.aba@pm.me>
2021-01-12 19:42:41 +0330

    chore: (wip) add register route and AuthForm component

---
+++ src/components/forms/AuthForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar color="primary" dark flat>
+      <v-toolbar-title>Register</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          label="Email"
+          name="email"
+          prepend-icon="mdi-email"
+          type="text"
+        >
+        </v-text-field>
+
+        <v-text-field
+          label="Password"
+          name="password"
+          prepend-icon="mdi-lock"
+          type="text"
+        >
+        </v-text-field>
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions class="pb-5">
+      <v-spacer> </v-spacer>
+      <v-btn color="primary">Register</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height blue-grey darken-3" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <auth-form></auth-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AuthForm from '@/components/forms/AuthForm.vue';
+export default {
+  components: {
+    AuthForm,
+  },
+};
+</script>
+
+<style></style>
