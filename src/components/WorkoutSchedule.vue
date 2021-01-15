72b49c68b3c7066f758dd3a929073c1f51cae3d3
pooriya <pooriya.aba@pm.me>
2021-01-15 17:29:54 +0330

    chore: (wip) add WorkoutSchedule component (UI) to home page

---
+++ src/components/WorkoutSchedule.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row justify="space-between">
+    <v-col cols="12" md="6">
+      <v-date-picker
+        v-model="date"
+        :events="functionEvent"
+        :prev-icon="prevIcon"
+        :next-icon="nextIcon"
+        full-width
+        color="accent"
+        elevation="1"
+      ></v-date-picker>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
+
+export default {
+  data: () => ({
+    prevIcon: mdiChevronLeft,
+    nextIcon: mdiChevronRight,
+    date: new Date().toISOString().substr(0, 10),
+    // TODO: replace it with response from API
+    scheduledSessions: ['2021-01-15', '2021-01-24', '2021-01-25'],
+    // TODO: replace it with response from API
+    missedSessions: ['2021-01-08', '2021-01-02'],
+  }),
+  methods: {
+    functionEvent(date) {
+      if (this.scheduledSessions.includes(date)) {
+        return 'blue';
+      }
+      if (this.missedSessions.includes(date)) {
+        return 'red';
+      }
+      return false;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <workout-schedule></workout-schedule>
+  </div>
 </template>
 
 <script>
+import WorkoutSchedule from '@/components/WorkoutSchedule.vue';
+
 export default {
   name: 'Home',
+  components: {
+    WorkoutSchedule,
+  },
 };
 </script>
