1c31fbab3f8d2137422ae6e27235ade6575109eb
pooriya <pooriya.aba@pm.me>
2021-01-15 14:53:42 +0330

    fix: ui structure for authentications routes and other routes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <v-app>
-    <app-drawer></app-drawer>
-    <app-bar></app-bar>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row>
-          <router-view></router-view>
-        </v-row>
-      </v-container>
-    </v-main>
+    <template v-if="!isAuthPath">
+      <app-drawer></app-drawer>
+      <app-bar></app-bar>
+      <v-main>
+        <v-container class="fill-height" fluid>
+          <v-row>
+            <router-view></router-view>
+          </v-row>
+        </v-container>
+      </v-main>
+    </template>
+    <template v-else>
+      <router-view></router-view>
+    </template>
     <app-snackbar />
   </v-app>
 </template>
@@ -21,5 +26,11 @@ import AppBar from '@/components/AppBar.vue';
 export default {
   name: 'App',
   components: { AppSnackbar, AppDrawer, AppBar },
+  computed: {
+    isAuthPath() {
+      const { name } = this.$route;
+      return name === 'Login' || name === 'Register' ? true : false;
+    },
+  },
 };
 </script>
