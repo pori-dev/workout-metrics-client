e62e4525c79b0944a9614d269ff5bc00e506dfc7
pooriya <pooriya.aba@pm.me>
2021-02-03 18:00:01 +0330

    feat: add weights chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <main-layout>
-    <workout-schedule-container />
+    <v-row>
+      <v-col cols="12" md="4">
+        <workout-schedule-container />
+      </v-col>
+      <v-col cols="12" md="8">
+        <weights-chart-container />
+      </v-col>
+    </v-row>
   </main-layout>
 </template>
 
 <script>
 import WorkoutScheduleContainer from '@/components/workout-schedule/WorkoutScheduleContainer.vue';
 import MainLayout from '@/layouts/MainLayout.vue';
+import WeightsChartContainer from '@/components/weight/WeightsChartContainer.vue';
 
 export default {
   name: 'Home',
   components: {
     MainLayout,
     WorkoutScheduleContainer,
+    WeightsChartContainer,
   },
 };
 </script>
