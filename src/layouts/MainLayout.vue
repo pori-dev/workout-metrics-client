c9d7ac90f4c85fd84a1e6a5da7790c7647f118d2
pooriya <pooriya.aba@pm.me>
2021-03-08 16:03:04 +0330

    refactor: move AppBar and AppDrawer components to layouts directory

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fill-height">
-    <app-drawer></app-drawer>
-    <app-bar app></app-bar>
+    <app-drawer />
+    <app-bar app />
     <v-main class="grey lighten-3 fill-height">
       <v-container fluid class="pa-8">
         <slot />
@@ -11,8 +11,8 @@
 </template>
 
 <script>
-import AppDrawer from '@/components/AppDrawer.vue';
-import AppBar from '@/components/AppBar.vue';
+import AppBar from './AppBar.vue';
+import AppDrawer from './AppDrawer.vue';
 
 export default {
   components: {
