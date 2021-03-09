4b833192e9e1a3d9c6317b7d892eaaeffbc4db2c
pooriya <pooriya.aba@pm.me>
2021-03-09 12:44:28 +0330

    refactor: add & use AppCard and ChartCard

--- src/components/schedule/generator/ScheduleGeneratorContainer.vue
+++ src/components/schedule/generator/ScheduleGeneratorContainer.vue
@@ -1,34 +1,36 @@
 <template>
-  <v-card height="100%">
-    <v-card-title class="font-weight-medium grey--text">
+  <app-card>
+    <template #title>
       Generate Recurring Schedules
-    </v-card-title>
+    </template>
 
-    <v-card-subtitle class="grey--text text--lighten-1 text-caption">
+    <template #subtitle>
       Generate to-do schedules for a specified duration
-    </v-card-subtitle>
+    </template>
 
-    <v-card-text>
+    <template #text>
       <schedule-generator @generate="generateSchedule" />
-    </v-card-text>
+    </template>
 
     <schedule-generator-dialog
       v-if="dialog"
       :dialog.sync="dialog"
       @generate-schedule="generateSchedule"
     />
-  </v-card>
+  </app-card>
 </template>
 
 <script>
 import { mapActions, mapMutations } from 'vuex';
 import ScheduleGenerator from './ScheduleGenerator.vue';
 import ScheduleGeneratorDialog from './ScheduleGeneratorDialog.vue';
+import AppCard from '@/components/common/AppCard.vue';
 
 export default {
   components: {
     ScheduleGenerator,
     ScheduleGeneratorDialog,
+    AppCard,
   },
 
   computed: {
